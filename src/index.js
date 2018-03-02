691a7af1d51155082b53df368cfcdb7c681f23f6
IharTsykaka <35345292+IharTsykaka@users.noreply.github.com>
2018-03-02 16:26:56 +0300

    task07

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zero = 0;
+  for (var i = 5; i <= number; i += 5){
+    var value = i;
+    while (value % 5 === 0){
+      zero += 1;
+      value = value / 5;
+    }
+  }
+  return zero;
 }
