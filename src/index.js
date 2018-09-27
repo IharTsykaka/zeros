d28423777f237117b997d06f439e18d4b542795d
IharTsykala <Sudja09@bk.ru>
2018-09-28 01:07:31 +0300

    2018g3better

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 module.exports = function getZerosCount(number) {
     let sum = 0;
-    let value = 0;
-    for (let i = 5; i <= number; i+=5) {
-      value = i;
-      while(value % 5 === 0) {
-          sum++;
-          value /= 5; 
-      }
-  }
+    let value = 5;
+//     for (let i = 5; i <= number; i+=5) {
+//       value = i;
+//       while(value % 5 === 0) {
+//           sum++;
+//           value /= 5; 
+//       }
+//   }
+    let counterPow = 1;
+    while(value < number) {
+         value *= counterPow;
+         counterPow++;
+    }
+    value = 5;
+    for (let i = 1; i < counterPow; i++) {
+        sum += Math.floor(number / Math.pow(value, i));
+    }
   return sum;
 }
