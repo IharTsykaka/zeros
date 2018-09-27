97c770035be4bfe4220ad542455723802eb79a1e
IharTsykala <Sudja09@bk.ru>
2018-09-27 12:05:49 +0300

    2018q3first

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 module.exports = function getZerosCount(number) {
-  var zero = 0;
-  for (var i = 5; i <= number; i += 5){
-    var value = i;
-    while (value % 5 === 0){
-      zero += 1;
-      value = value / 5;
-    }
+    let sum = 0;
+    let value = 0;
+    for (let i = 5; i <= number; i+=5) {
+      value = i;
+      while(value % 5 === 0) {
+          sum++;
+          value /= 5; 
+      }
   }
-  return zero;
+  return sum;
 }
